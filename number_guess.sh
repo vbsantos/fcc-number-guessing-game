eee330aaa319d76c51f48c98082f13467b561250
Vinícius Bohrer dos Santos <vinicius.bohrer@voalle.com.br>
2023-03-28 23:18:41 -0300

    fix: script and sql

--- number_guess.sh
+++ number_guess.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
 
 # postgres base command
-declare PSQL="psql --username=freecodecamp --dbname=number_guessing_game -t --no-align -c"
+PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
 
 # Generate random number between 1 to 1000
-declare secret_number=$((RANDOM % 1000 + 1))
+secret_number=$((RANDOM % 1000 + 1))
 
 # Prompt user for username
-read -p "Enter your username: " username
+echo "Enter your username: "
+read username
 
 # Check if user already exists in the database
-declare user_exists="$($PSQL "SELECT * FROM users WHERE username = '${username}';")"
+user_exists="$($PSQL "SELECT * FROM users WHERE username = '${username}';")"
 
 if [ -n "${user_exists}" ]; then
     # Extract data for existing user
     IFS='|' read -r -a arr <<< "$user_exists"
-    declare number_of_games="${arr[2]}"
-    declare best_game="${arr[3]}"
+    number_of_games="${arr[2]}"
+    best_game="${arr[3]}"
 
     # Welcome back message
     echo "Welcome back, ${username}! You have played ${number_of_games} games, and your best game took ${best_game} guesses."
@@ -25,7 +26,9 @@ else
     echo "Welcome, ${username}! It looks like this is your first time here."
     
     # Add user to the database
-    declare create_user="$($PSQL "INSERT INTO users (username,number_of_games,best_game) VALUES ('${username}',0,1000);")"
+    create_user="$($PSQL "INSERT INTO users (username,number_of_games,best_game) VALUES ('${username}',0,1000);")"
+    number_of_games="0"
+    best_game="1000"
 fi
 
 # Ask user to guess the secret number
